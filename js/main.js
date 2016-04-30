4fc729ef8d0dc2372f3154f0dc00c06ab24618be
LJ <ljnotes@outlook.com>
2016-04-30 17:39:26 +0300

    Include the first version of the project - HTML5 Boilerplate, jQuery, bootstrap and Knockout libraries included

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*!
+ * Author: LJ
+ * Date: 30-April-2016
+ * Version: 1.0
+ * Description: Holds the main JS functions associated with the application. 
+ */
+
+/*!
+ * Define the global object - ljnotes 
+ */
+var ljnotes = {
+    app: "sme", // spell-me-out
+    version: 1
+};
+
+/*!
+ * Do the initializations 
+ */
+(function(globalObject) {
+    
+    
+    
+})(ljnotes);
---
+++ js/smedata.js
@@ -0,0 +1,44 @@
+/*!
+ * Author: LJ
+ * Date: 30-April-2016
+ * Version: 1.0
+ * Description: Holds the data for Spell-me-out app
+ */
+
+// Set the data for the application. 
+(function(globalObj){
+    
+    var smeData = {
+        a: {spell: "Alpha"}, 
+        b: {spell: "Bravo"}, 
+        c: {spell: "Charlie"},
+        d: {spell: "Delta"}, 
+        e: {spell: "Echo"}, 
+        f: {spell: "Foxtrot"}, 
+        g: {spell: "Golf"}, 
+        h: {spell: "Hotel"}, 
+        i: {spell: "India"}, 
+        j: {spell: "Juliet"}, 
+        k: {spell: "Kilo"}, 
+        l: {spell: "Lima"}, 
+        m: {spell: "Mike"}, 
+        n: {spell: "November"}, 
+        o: {spell: "Oscar"}, 
+        p: {spell: "Papa"}, 
+        q: {spell: "Quebec"}, 
+        r: {spell: "Romeo"}, 
+        s: {spell: "Sierra"}, 
+        t: {spell: "Tango"}, 
+        u: {spell: "Uniform"}, 
+        v: {spell: "Victor"}, 
+        w: {spell: "Whisky"}, 
+        x: {spell: "X-ray"}, 
+        y: {spell: "Yankee"}, 
+        z: {spell: "Zulu"}
+    };
+    
+    // Set the data to the object
+    if(globalObj) {
+       globalObj.smeData = smeData; 
+    }
+})(window.ljnotes);
